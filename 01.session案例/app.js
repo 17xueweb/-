afeb9268cf15eaaa18f497437585d0d70c9b5d3c
lcruan <1416802331@qq.com>
2024-01-29 20:07:21 +0800

    获取session中数据

--- 01.session案例/app.js
+++ 01.session案例/app.js
@@ -24,7 +24,7 @@ app.post('/api/login', (req, res) => {
   }
 
   // TODO_02：请将登录成功后的用户信息，保存到 Session 中
-  // 注意：只有成功匹配了 express-session 这个中间件之后，才能通过 req 点出来 session 这个属性
+  // 注意：只有成功匹配了 express-session 这个中间件之后，才能通过 req 点出来 session 这个属性 
   req.session.user = req.body // 用户的信息
   req.session.islogin = true // 用户的登录状态
 
@@ -34,6 +34,14 @@ app.post('/api/login', (req, res) => {
 // 获取用户姓名的接口
 app.get('/api/username', (req, res) => {
   // TODO_03：请从 Session 中获取用户的名称，响应给客户端
+  if (!req.session.islogin) {
+    return res.send({ status : 1, msg: 'fail'})
+  }
+  res.send({
+    status: 0,
+    msg: 'success',
+    username: req.session.user.username
+  })
 })
 
 // 退出登录的接口

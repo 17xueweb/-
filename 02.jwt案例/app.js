3ae2838934ac9655021a164b439b3ab70a7684ee
lcruan <1416802331@qq.com>
2024-01-29 21:33:22 +0800

    使用req.user获取express-jwt还原的用户信息

--- 02.jwt案例/app.js
+++ 02.jwt案例/app.js
@@ -5,7 +5,7 @@ const app = express()
 
 // TODO_01：安装并导入 JWT 相关的两个包，分别是 jsonwebtoken 和 express-jwt
 const jwt = require('jsonwebtoken') // 将用户信息生成token字符串
-const expressJWT = require('express-jwt') // 将t 还原成用户信息
+const expressJWT = require('express-jwt') // 将jwt 还原成用户信息
 
 // 允许跨域资源共享
 const cors = require('cors')
@@ -18,8 +18,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // TODO_02：定义 secret 密钥，建议将密钥命名为 secretKey
 const secretKey = 'itheima No1 ^_^'
 
-// TODO_04：注册将 JWT 字符串解析还原成 JSON 对象的中间件
-app.use(expressJWT({ secret: secretKey }).unless( { path: [/^\/api\//]}))
+// TODO_04：注册将 JWT 字符串解析还原成 JSON 对象的中间件 
+// 注意：只要配置成功了 express-jwt 这个中间件，就可以把解析出来的用户信息，挂载到 req.user 属性上
+app.use(expressJWT({ secret: secretKey }).unless({ path: [/^\/api\//]}))
 
 // 登录接口
 app.post('/api/login', function (req, res) {
@@ -37,6 +38,7 @@ app.post('/api/login', function (req, res) {
   // 参数1：用户的信息对象
   // 参数2：加密的秘钥
   // 参数3：配置对象，可以配置当前 token 的有效期
+  // 记住：千万不要把密码加密到 token 字符中
   const tokenStr = jwt.sign({username: userinfo.username}, secretKey, { expiresIn: '30s'})
   res.send({
     status: 200,
@@ -48,10 +50,11 @@ app.post('/api/login', function (req, res) {
 // 这是一个有权限的 API 接口
 app.get('/admin/getinfo', function (req, res) {
   // TODO_05：使用 req.user 获取用户信息，并使用 data 属性将用户信息发送给客户端
+  console.log(req.user);
   res.send({
     status: 200,
     message: '获取用户信息成功！',
-    data: {} // 要发送给客户端的用户信息
+    data: req.user // 要发送给客户端的用户信息
   })
 })
 
